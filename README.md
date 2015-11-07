da7110aba146361915b6f4e2e1f8ad57e1c87b85
Petr Hejna <Achse0@gmail.com>
2015-11-07 12:39:42 +0100

    travis, scrutinizer added

---
+++ README.md
@@ -0,0 +1,3 @@
+![](https://travis-ci.org/Achse/nette-date-time-input.svg?branch=master)
+![](https://scrutinizer-ci.com/g/Achse/nette-date-time-input/badges/quality-score.png?b=master)
+![](https://scrutinizer-ci.com/g/Achse/nette-date-time-input/badges/coverage.png?b=master)
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,15 @@ namespace Achse\DateTimeInput\Test;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Tester\Environment;
+
+
+
 if (!class_exists('Tester\Assert')) {
 	echo "Install Nette Tester using `composer update --dev`\n";
 	exit(1);
 }
 
+date_default_timezone_set('Europe/Prague');
+
+Environment::setup();
